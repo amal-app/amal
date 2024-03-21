59ef657e5b479e0f65c37cd531bc2cbd1de63042
Arham4 <arhamjsiddiqui@gmail.com>
2024-03-21 01:41:25 +0000

    [#3] Fix imports

--- assets/images/index.tsx
+++ assets/images/index.tsx
@@ -1,8 +1,7 @@
-import AMAL_LOGO from './images/amal-logo.png';
-import HOME_SCREEN from './images/home_screen.png';
-import QURAN from './images/quran.png';
-import PRAYING from './images/praying.png';
-import KNOWLEDGE from './images/knowledge.png';
-import { ImageSourcePropType } from 'react-native';
+import AMAL_LOGO from './amal-logo.png';
+import HOME_SCREEN from './home_screen.png';
+import QURAN from './quran.png';
+import PRAYING from './praying.png';
+import KNOWLEDGE from './knowledge.png';
 
 export { AMAL_LOGO, HOME_SCREEN, QURAN, PRAYING, KNOWLEDGE };
